c986457f2eae4bddc0170dd4b143001ec6c63cd4
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2023-06-15 12:23:13 -0400

    first sharing of the base code

---
+++ components/index.ts
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import CustomFilters from "./CustomFilters";
+import SearchBar from "./SearchBar";
+import CarCard from "./CarCard";
+import SearchManufacture from "./SearchManufacture";
+import CarDetails from "./CarDetails";
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  CustomFilters,
+  SearchBar,
+  SearchManufacture,
+  CarCard,
+  CarDetails
+};
---
+++ utils/index.ts
@@ -0,0 +1,76 @@
+import { CarProps } from "@/types";
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '0';
+export async function fetchCars() {
+ 
+
+  const headers = {
+    "X-RapidAPI-Key": "bd51b957c5msh474524f1fb07f36p11bc9cjsnd0bce0c42395",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera",
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+  return result;
+}
+
+export const calculateCarRent = (
+  city_mpg: number,
+  year: number,
+  currency: string
+) => {
+  const basePricePerDay = 50; //Base rental price per day in dollars
+
+  const mileageFactor = 0.1; //Aditional rate per mile driven
+
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  const mileageRate = city_mpg * mileageFactor;
+
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  const totalRentalRatePerDay = convertCurrency(currency, rentalRatePerDay);
+  return totalRentalRatePerDay;
+};
+
+const convertCurrency = (currency: string, totalRentalRatePerDay: number) => {
+  const USD = 1;
+  const DOP = 54.75;
+  const EUR = 0.92;
+
+  switch (currency) {
+    case "USD":
+      totalRentalRatePerDay *= USD;
+      break;
+    case "DOP":
+      totalRentalRatePerDay *= DOP;
+      break;
+    case "EUR":
+      totalRentalRatePerDay *= EUR;
+      break;
+    default:
+      "currency unavalible";
+      break;
+  }
+  return totalRentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+  return `${url}`;
+};
